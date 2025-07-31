51cc93609c70679e54efc850c0c1de2660580838
CristianT2 <c.torrejon96@gmail.com>
2025-07-31 20:55:41 -0300

    Se creo el proyecto

---
+++ src/main/java/ar/edu/unju/fi/sistematodolist/SistemaTodolistApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.sistematodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTodolistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaTodolistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ar/edu/unju/fi/sistematodolist/SistemaTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.sistematodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTodolistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
